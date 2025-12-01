b53495def02a4878e1ed28c52af3e8e366dd67b6
orosmatthew <mjo1@clevelandmetroparks.com>
2025-12-01 10:14:32 -0500

    Add day 1 part 1

--- day01-part1/main.cpp
+++ day01-part1/main.cpp
@@ -2,16 +2,33 @@
 
 #include <print>
 
+static int parse_rotation(const std::string& data, size_t& pos)
+{
+    const int sign = data[pos] == 'L' ? -1 : 1;
+    ++pos;
+    const int value = parse_int(data, pos);
+    return sign * value;
+}
+
 static int solve(const std::string& data)
 {
-    return 1;
+    int zero_count = 0;
+    int dial = 50;
+    for (size_t pos = 0; pos < data.length(); ++pos) {
+        const int rotation = parse_rotation(data, pos);
+        dial = math_mod(dial + rotation, 100);
+        if (dial == 0) {
+            ++zero_count;
+        }
+    }
+    return zero_count;
 }
 
 int main()
 {
-    const std::string data = read_file("./day01-part1/sample.txt");
+    const std::string data = read_file("./day01-part1/input.txt");
 #ifdef BENCHMARK
-    benchmark([&] { solve(data); }, 1000);
+    benchmark([&] { return solve(data); }, 10000);
 #else
     std::println("{}", solve(data));
 #endif
--- utils.hpp
+++ utils.hpp
@@ -20,10 +20,35 @@ void benchmark(Func func, const int runs)
     double time_running_total = 0.0;
     for (int i = 0; i < runs; ++i) {
         auto start = std::chrono::high_resolution_clock::now();
-        std::invoke(func);
+        volatile auto result = std::invoke(func);
         auto end = std::chrono::high_resolution_clock::now();
         time_running_total += std::chrono::duration<double, std::nano>(end - start).count();
     }
     int avg_ns = static_cast<int>(std::round(time_running_total / runs));
-    std::println("Average ns: {}", avg_ns);
+    std::println("Iterations: {}, Average ns: {}", runs, avg_ns);
+}
+
+inline bool is_digit(const char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+inline int parse_int(const std::string& data, size_t& pos)
+{
+    int result = 0;
+    while (is_digit(data[pos])) {
+        result = result * 10 + (data[pos] - '0');
+        ++pos;
+    }
+    return result;
+}
+
+template <typename Int>
+constexpr Int math_mod(const Int dividend, const Int divisor)
+{
+    const Int result = dividend % divisor;
+    if ((result < 0 && divisor > 0) || (result > 0 && divisor < 0)) {
+        return result + divisor;
+    }
+    return result;
 }
